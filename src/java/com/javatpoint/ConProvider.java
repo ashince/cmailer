d7ce38d1b6b8dfcdc08bec7c73428aee853d2bff
ashince <ashincekd@gmail.com>
2018-01-05 11:03:28 +0530

    initial

--- src/java/com/javatpoint/ConProvider.java
+++ src/java/com/javatpoint/ConProvider.java
@@ -5,14 +5,14 @@ public class ConProvider {
 	public static Connection getConnection(){
 	Connection con=null;
 	try{
-		/*Class.forName(System.getenv("SQL_DRIVER"));
+		Class.forName(System.getenv("SQL_DRIVER"));
 		String url = System.getenv("SQL_URL");;
 	    String user= System.getenv("SQL_USER");
 	    String pwd = System.getenv("SQL_PASSWORD"); 
-	    con=DriverManager.getConnection(url,user,pwd);*/
+	    con=DriverManager.getConnection(url,user,pwd);
 	    
-	    Class.forName("com.mysql.jdbc.Driver");
-		con=DriverManager.getConnection("jdbc:mysql://10.10.242.22:3306/ashish","ashish","ashish");
+	    /*Class.forName("com.mysql.jdbc.Driver");
+		con=DriverManager.getConnection("jdbc:mysql://10.10.242.22:3306/ashish","ashish","ashish");*/
 	    
 		
 	}catch(Exception e){System.out.println(e);}
